dd645795a0d691e8fbd0238177fad27a2ccff4d6
CXO2 <com@cxo2.me>
2026-07-14 02:21:38 +0900

    Revert "Update exception base"
    This reverts commit 7759b39a9a58e89aeafae1e96238b6ec4d71fb60.

--- include/Genode/System/Exception.hpp
+++ include/Genode/System/Exception.hpp
@@ -5,10 +5,10 @@
 
 namespace Gx
 {
-    class Exception : public std::exception
+    class Exception : public std::runtime_error
     {
     public:
-        explicit Exception(const std::string& message) : std::exception(message.c_str()) {};
+        explicit Exception(const std::string& message) : std::runtime_error(message) {};
     };
 
     class NotSupportedException : public Exception
